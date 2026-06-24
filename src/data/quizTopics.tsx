e7df7274dcc4ce12fc1718933fb962ba49305a71
mary1na-code <zioladesino@gmail.com>
2026-06-24 03:33:55 -0700

    feat: comment out unused imports and icon properties in quizTopics

--- src/data/quizTopics.tsx
+++ src/data/quizTopics.tsx
@@ -1,66 +1,66 @@
-import { ReactNode } from 'react'
-import Angular from '../assets/icons/angular.svg?react'
-import CSS from '../assets/icons/css-3.svg?react'
-import Django from '../assets/icons/dj.svg?react'
-import Gatsby from '../assets/icons/gatsby.svg?react'
-import JavaScript from '../assets/icons/javascript.svg?react'
-import Kotlin from '../assets/icons/kotlin.svg?react'
-import Laravel from '../assets/icons/laravel.svg?react'
-import Python from '../assets/icons/python.svg?react'
-import ReactIcon from '../assets/icons/react.svg?react'
-import BulbIcon from '../assets/icons/bulb.svg?react'
+// import { ReactNode } from 'react'
+// import Angular from '../assets/icons/angular.svg?react'
+// import CSS from '../assets/icons/css-3.svg?react'
+// import Django from '../assets/icons/dj.svg?react'
+// import Gatsby from '../assets/icons/gatsby.svg?react'
+// import JavaScript from '../assets/icons/javascript.svg?react'
+// import Kotlin from '../assets/icons/kotlin.svg?react'
+// import Laravel from '../assets/icons/laravel.svg?react'
+// import Python from '../assets/icons/python.svg?react'
+// import ReactIcon from '../assets/icons/react.svg?react'
+// import BulbIcon from '../assets/icons/bulb.svg?react'
 
 type QuizTopic = {
   title: string
-  icon: ReactNode
+  // icon: ReactNode
   disabled?: boolean
 }
 
 export const quizTopics: QuizTopic[] = [
   {
     title: 'GST 112',
-    icon: <ReactIcon />,
+    // icon: <ReactIcon />,
   },
   {
     title: 'GST 114',
-    icon: <JavaScript />,
+    // icon: <JavaScript />,
   },
   {
     title: 'COS 112',
-    icon: <Python />,
+    // icon: <Python />,
   },
   {
     title: 'COS 114',
-    icon: <BulbIcon />,
+    // icon: <BulbIcon />,
   },
   {
     title: 'COS 116',
-    icon: <Gatsby />,
+    // icon: <Gatsby />,
     disabled: true,
   },
   {
     title: 'COS 118',
-    icon: <Angular />,
+    // icon: <Angular />,
     disabled: true,
   },
   {
     title: 'MTH 102',
-    icon: <Django />,
+    // icon: <Django />,
     disabled: true,
   },
   {
     title: 'COS 102',
-    icon: <CSS />,
+    // icon: <CSS />,
     disabled: true,
   },
   {
     title: 'PHY 102',
-    icon: <Kotlin />,
+    // icon: <Kotlin />,
     disabled: true,
   },
   {
     title: 'PHY 108',
-    icon: <Laravel />,
+    // icon: <Laravel />,
     disabled: true,
   },
 ]
